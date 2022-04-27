2cec364133fb4cab631de766b01966bd89d4deec
cleitonBarros <48657479+cleitonBarros@users.noreply.github.com>
2022-04-27 12:43:34 -0300

    commit
    adição de JS pra trocar svg lock e mail

--- script/main.js
+++ script/main.js
@@ -1,14 +1,29 @@
-const senha  =  document.getElementById('senha');
+const inputSenha  =  document.getElementById('senha');
 const view  = document.getElementById('password-view')
+const lock = document.getElementById('lock')
+const inputEmail = document.getElementById('input-email')
+const imgEmail= document.getElementById('email')
 
+inputSenha.addEventListener('focus',()=>{
+    lock.src='../images/lock-focus.svg'
+},true)
+inputEmail.addEventListener('focus', ()=>{
+    imgEmail.src='../images/mail-focus.svg'
+},true)
+
+window.addEventListener('blur',()=>{
+    lock.src='../images/lock.svg'
+    imgEmail.src='../images/mail.svg'
+},true)
 
 view.addEventListener('click',( )=>{
-    if(senha.type =='password'){
-        senha.type = 'text'
+    if(inputSenha.type =='password'){
+        inputSenha.type = 'text'
         view.src = '../images/eye.svg'
+
     }
-    else if(senha.type == 'text'){
-        senha.type ='password'
+    else if( inputSenha.type == 'text'){
+        inputSenha.type ='password'
         view.src ='../images/eye-focus2.svg'
     }
     else return
